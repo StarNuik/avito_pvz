3ea73ce36d69a7e935e39a56d4f074e3d4d41991
Nikolai E <starnuik@gmail.com>
2025-04-10 14:51:18 +0300

    Метод и тест закрытия приемки

--- pkg/usecase/close_last_reception.go
+++ pkg/usecase/close_last_reception.go
@@ -5,9 +5,31 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/starnuik/avito_pvz/pkg/entity"
+	"github.com/starnuik/avito_pvz/pkg/repository"
 	"github.com/starnuik/avito_pvz/pkg/token"
 )
 
 func (u *usecase) CloseLastReception(ctx context.Context, token token.Payload, pvzId uuid.UUID) (entity.Reception, error) {
-	panic("")
+	if token.UserRole != entity.RoleEmployee {
+		return entity.Reception{}, entity.ErrUnauthorized
+	}
+
+	tx, err := u.repo.LockPvz(ctx, pvzId, repository.LockNoWrites)
+	if err != nil {
+		return entity.Reception{}, err
+	}
+	defer tx.Rollback()
+
+	reception, err := u.repo.GetOpenReception(ctx, pvzId)
+	// includes entity.ErrNotFound
+	if err != nil {
+		return entity.Reception{}, err
+	}
+
+	reception, err = u.repo.UpdateReceptionStatus(ctx, reception.Id, entity.StatusClose)
+	if err != nil {
+		return entity.Reception{}, err
+	}
+
+	return reception, tx.Commit()
 }
---
+++ pkg/usecase/close_last_reception_test.go
@@ -0,0 +1,57 @@
+package usecase_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/starnuik/avito_pvz/pkg/entity"
+	"github.com/starnuik/avito_pvz/pkg/mocks"
+	"github.com/starnuik/avito_pvz/pkg/repository"
+	"github.com/starnuik/avito_pvz/pkg/token"
+	"github.com/starnuik/avito_pvz/pkg/usecase"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func Test_CloseLastReception(t *testing.T) {
+	// Arrange
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+
+	reception := entity.Reception{
+		Id:       uuid.Max,
+		PvzId:    uuid.Max,
+		Status:   entity.StatusClose,
+		DateTime: time.Unix(1000, 0),
+	}
+
+	tx := mocks.NewMockTx(ctrl)
+	tx.EXPECT().Rollback().After(tx.EXPECT().Commit())
+
+	repo := mocks.NewMockRepository(ctrl)
+	repo.EXPECT().
+		LockPvz(gomock.Any(), reception.PvzId, repository.LockNoWrites).
+		Return(tx, nil)
+	repo.EXPECT().
+		GetOpenReception(gomock.Any(), reception.PvzId).
+		Return(reception, nil)
+	repo.EXPECT().
+		UpdateReceptionStatus(gomock.Any(), reception.Id, entity.StatusClose).
+		Return(reception, nil)
+
+	usecase := usecase.New(repo, nil, nil)
+
+	ctx := context.Background()
+	token := token.Payload{
+		UserRole: entity.RoleEmployee,
+	}
+
+	// Act
+	result, err := usecase.CloseLastReception(ctx, token, reception.PvzId)
+
+	// Assert
+	require.Nil(err)
+	require.Equal(reception, result)
+}
